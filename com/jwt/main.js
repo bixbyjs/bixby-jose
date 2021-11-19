cbacad8495a2428a6b09e8022fe868b4fb30f5d7
Jared Hanson <jaredhanson@gmail.com>
2021-11-19 12:53:23 -0800

    Add key argument to jwt.sign.

--- com/jwt/main.js
+++ com/jwt/main.js
@@ -3,38 +3,16 @@ exports = module.exports = function() {
   
   return {
     
-    sign: function(claims, cb) {
-      console.log('SIGN THE JWT');
-      console.log(claims)
-      
-      jose.generateKeyPair('RS256')
-        .then(function(o) {
-          console.log(o);
-          console.log(o.privateKey)
-          console.log(o.publicKey)
-          
-          return new jose.SignJWT(claims)
-            .setProtectedHeader({ alg: 'RS256' })
-            //.setIssuedAt()
-            //.setIssuer('urn:example:issuer')
-            //.setAudience('urn:example:audience')
-            //.setExpirationTime('2h')
-            .sign(o.privateKey)
-          
-          //return cb(null, 'jwt');
-        })
+    sign: function(claims, key, cb) {
+      return new jose.SignJWT(claims)
+        .setProtectedHeader({ alg: 'RS256' })
+        .sign(key)
         .then(function(jwt) {
-          console.log(jwt)
-          
           return cb(null, jwt);
         })
         .catch(function(error) {
-          console.log(error);
-          
           return cb(error);
-        })
-      
-      
+        });
     }
     
   };
