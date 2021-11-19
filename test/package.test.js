63fdea7b875df62672d4909fd2259d31c79cbbcf
Jared Hanson <jaredhanson@gmail.com>
2021-11-19 13:34:53 -0800

    Update package metadata.

---
+++ test/package.test.js
@@ -0,0 +1,15 @@
+/* global describe, it */
+
+var expect = require('chai').expect;
+
+
+describe('package.json', function() {
+  var json = require('../package.json');
+  
+  it('should have assembly metadata', function() {
+    expect(json.assembly.namespace).to.equal('jose');
+    expect(json.assembly.components).to.deep.equal([
+      'jwt/main'
+    ]);
+  });
+});
